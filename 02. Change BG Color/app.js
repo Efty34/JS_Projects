4d9223ef2e34c04f071314ba3b08d877c2f4744a
Efty34 <efty17.hasan@gmail.com>
2024-07-05 12:15:19 +0600

    02. Change BG Color

---
+++ 02. Change BG Color/app.js
@@ -0,0 +1,36 @@
+const btns = document.querySelectorAll(".btn");
+const body = document.querySelector("body");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const color = btn.value;
+    changeBgColor(color);
+  });
+});
+
+function changeBgColor(color) {
+  body.className = "";
+  switch (color) {
+    case "purple":
+      body.classList.add("purple");
+      break;
+    case "blue":
+      body.classList.add("blue");
+      break;
+    case "red":
+      body.classList.add("red");
+      break;
+    case "green":
+      body.classList.add("green");
+      break;
+    case "yellow":
+      body.classList.add("yellow");
+      break;
+    case "teal":
+      body.classList.add("teal");
+      break;
+
+    default:
+      break;
+  }
+}
